e55b5bf93d0717292daa3a5c88d17f4983640361
samba325 <asiva325@gmail.com>
2017-11-26 20:39:28 +0530

    db update

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 if(process.env.NODE_ENV === 'production') {
 module.exports = {
-    mongoURI: 'mongodb://<samba425>:<samba425>@ds157288.mlab.com:57288/taskmanager'
+    mongoURI: 'mongodb://samba425:samba425@ds157288.mlab.com:57288/taskmanager'
 }
 } else {module.exports = {
     mongoURI: 'mongodb://localhost:27017/taskmanager'
